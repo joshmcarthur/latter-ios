0533ca36c4e1b78aa69ad9356ad70bc50ae32684
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-03 23:58:31 +1200

    Stop setting up the view from the controller - view must be adjusted to set itself up

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -9,7 +9,6 @@ class PlayerController < UIViewController
     super
 
     @player_container = PlayerView.alloc.initWithPlayerAndFrame(@player, [[0, 0], [self.view.frame.size.width, self.view.frame.size.height]])
-    @player_container.setup
     self.view.addSubview(@player_container)
   end
 
