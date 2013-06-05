930dc6c1064544c8222c837c3a105b5bb15afb8e
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-05 12:45:05 +1200

    Add challenges controller to tab view navigation

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,7 +5,6 @@ class AppDelegate
     App::Persistence['api_endpoint'] = 'https://latter.herokuapp.com' unless App::Persistence['api_endpoint']
     App::Persistence['api_version'] = 'v1' unless App::Persistence['api_version']
 
-
     if App::Persistence["auth_token"].nil?
       root_controller = AuthKeyController.alloc.initWithForm(AuthKeyController.build_form)
     else
@@ -13,13 +12,14 @@ class AppDelegate
     end
 
     games_controller = GamesController.alloc.initWithStyle(UITableViewStylePlain)
+    challenges_controller = GamesController.alloc.initWithStyle(UITableViewStylePlain)
 
     navigation_controller = UINavigationController.alloc.initWithRootViewController(root_controller)
     navigation_controller.wantsFullScreenLayout = true
     navigation_controller.toolbarHidden = true
 
     tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [navigation_controller, games_controller]
+    tab_controller.viewControllers = [navigation_controller, games_controller, challenges_controller]
     @window.rootViewController = tab_controller
     @window.makeKeyAndVisible
     true
