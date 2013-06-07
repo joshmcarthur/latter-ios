22ee31605ec184eadd4c81779d8371fbfffc2e03
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-07 16:39:02 +1200

    Add constants to refer to challenges controller sections correctly

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,7 +1,67 @@
 class ChallengesController < UITableViewController
+
+  YOUR_CHALLENGES_SECTION = 0
+  ALL_CHALLENGES_SECTION  = 1
+
   def initWithStyle(style)
     super
     self.tabBarItem = UITabBarItem.alloc.initWithTitle("Challenges", image: UIImage.imageNamed("challenges.png"), tag: 3)
     self
+  end
+
+
+  def tableView(tableView, titleForHeaderInSection: section)
+    if section == YOUR_CHALLENGES_SECTION
+      "Your Challenges"
+    elsif section == ALL_CHALLENGES_SECTION
+      "All Challenges"
+    end
+  end
+
+  def viewDidLoad
+    @challenges = []
+    view.dataSource = view.delegate = self
+
+    Latter::API.new.get("/games", {complete: "false", per_page: "25"}) do |response|
+      if response.ok?
+        json = BubbleWrap::JSON.parse(response.body.to_s)
+        @challenges = json.select { |hash| hash["challenger"] && hash["challenged"] }.map { |game_hash| Game.new(game_hash) }
+        view.reloadData
+      else
+        $stderr.puts response.status_code
+        App.alert(response.error_message)
+      end
+    end
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    return case indexPath.section
+           when ALL_CHALLENGES_SECTION
+            ChallengeCell.cellForChallenge(@challenges[indexPath.row], involvesCurrentPlayer: false, inTableView:tableView)
+           end
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    return case indexPath.section
+           when ALL_CHALLENGES_SECTION
+            ChallengeCell.cellForChallenge(@challenges[indexPath.row], involvesCurrentPlayer: false, inTableView:tableView)
+           end
+  end
+
+  def tableView(tableView, heightForRowAtIndexPath: indexPath)
+    # Arbitrary
+    64 + 10
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    if section == ALL_CHALLENGES_SECTION
+      @challenges.count
+    else
+      0
+    end
+  end
+
+  def numberOfSectionsInTableView(tableView)
+    2
   end
 end
