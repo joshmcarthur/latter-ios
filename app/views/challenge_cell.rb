abc9b1231a64199ddf0a324229d22a3b26c7eb0b
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-07 16:38:37 +1200

    Add ChallengeCell view class

---
+++ app/views/challenge_cell.rb
@@ -0,0 +1,35 @@
+class ChallengeCell < UITableViewCell
+  CellID = 'ChallengeCell'
+
+  def self.cellForChallenge(challenge, involvesCurrentPlayer:ours, inTableView:tableView)
+    cell = tableView.dequeueReusableCellWithIdentifier(ChallengeCell::CellID) || ChallengeCell.alloc.initWithStyle(UITableViewCellStyleValue1, reuseIdentifier:CellID)
+    cell.indentationWidth = 0
+    cell.fillWithChallenge(challenge, inTableView:tableView)
+    cell.appendCompletionAccessory if ours
+    cell
+  end
+
+  def fillWithChallenge(challenge, inTableView: tableView)
+    @challenger_image = PlayerGravatar.alloc.initWithImageAndFrame(
+      challenge.challenger.load_gravatar_image,
+      [[5, 5], [64, 64]]
+    ) if challenge.challenger
+
+
+
+    @challenged_image = PlayerGravatar.alloc.initWithImageAndFrame(
+      challenge.challenged.load_gravatar_image,
+      [[5, 5], [64, 64]]
+    ) if challenge.challenged
+
+    self.contentView.addSubview(@challenger_image)
+    self.contentView.addSubview(@challenged_image)
+  end
+
+  def layoutSubviews
+    super
+
+    cell_bounds = self.contentView.frame.size
+    @challenged_image.frame = CGRectMake(cell_bounds.width - (64 + 5), 5, 64, 64)
+  end
+end
