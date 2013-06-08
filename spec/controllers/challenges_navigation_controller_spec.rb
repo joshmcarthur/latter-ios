45295bf3696d94d30176fec919f88893588d7553
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-09 00:18:19 +1200

    Re-enable some specs, but seem to be catching a failure from another spec file here

--- spec/controllers/challenges_navigation_controller_spec.rb
+++ spec/controllers/challenges_navigation_controller_spec.rb
@@ -1,15 +1,16 @@
 describe "Challenge navigation controller" do
   extend WebStub::SpecHelpers
   before do
-    disable_network_access!
+    stub_request("#{App::Persistence['api_endpoint']}/api/v1/games.json?auth_token=testing&complete=false").
+      to_return(json: [])
     self.controller = ChallengeNavigationController.alloc.init
   end
 
   tests ChallengeNavigationController
 
-  # it "inherits from UINavigationController" do
-  #   self.controller.superclass.should.equal UINavigationController
-  # end
+  it "inherits from UINavigationController" do
+    self.controller.superclass.should.equal UINavigationController
+  end
 
   # it "sets the challenges controller as it's root view controller" do
   #   self.controller.viewControllers.first.class.should.equal ChallengesController
