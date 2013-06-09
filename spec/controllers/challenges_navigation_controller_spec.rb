d71b08bce1e99a5587418154b66cd83fa5f1f9bb
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-09 23:17:43 +1200

    Passing specs (locally, at least)

--- spec/controllers/challenges_navigation_controller_spec.rb
+++ spec/controllers/challenges_navigation_controller_spec.rb
@@ -1,18 +1,15 @@
 describe "Challenge navigation controller" do
   extend WebStub::SpecHelpers
   before do
-    stub_request("#{App::Persistence['api_endpoint']}/games.json?auth_token=testing&complete=false").
-      to_return(json: [])
-    self.controller = ChallengeNavigationController.alloc.init
+    @controller = ChallengeNavigationController.alloc.init
   end
 
-  tests ChallengeNavigationController
 
   it "inherits from UINavigationController" do
-    self.controller.superclass.should.equal UINavigationController
+    @controller.superclass.should.equal UINavigationController
   end
 
-  # it "sets the challenges controller as it's root view controller" do
-  #   self.controller.viewControllers.first.class.should.equal ChallengesController
-  # end
+  it "sets the challenges controller as it's root view controller" do
+    @controller.viewControllers.first.class.should.equal ChallengesController
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,4 @@
-qdescribe 'Game model' do
+describe 'Game model' do
   # A game between Mike and Josh, Mike won 21-19
   def game_hash
     Fixture.load("games.json").first
